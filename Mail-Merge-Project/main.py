82eeee97e1ab48d49051c96903f51f80d5b7c533
ibheelz <ibheelz@icloud.com>
2024-03-10 00:30:39 +0100

    Added Relative Paths

--- Mail-Merge-Project/main.py
+++ Mail-Merge-Project/main.py
@@ -1,7 +1,4 @@
 #TODO: Create a letter using starting_letter.txt 
-
-with open ("starting_letter.txt") as letter:
-
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
@@ -9,3 +6,22 @@ with open ("starting_letter.txt") as letter:
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+import os
+
+name_content = open ("Mail-Merge-Project/Input/Names/invited_names.txt")
+names = (name_content.readlines())
+
+for i in range(len(names)):
+    names[i]=names[i].replace('\n','')
+
+letter_content = open ("Mail-Merge-Project/Input/Letters/starting_letter.txt")
+letter = (letter_content.read())
+
+for i in names:
+    x = letter.replace("[name]", i)
+    y = open(f"Mail-Merge-Project/Output/ReadyToSend/{i}.txt", "w")
+    z = y.write(x)
+  
+
+# print(letter)
